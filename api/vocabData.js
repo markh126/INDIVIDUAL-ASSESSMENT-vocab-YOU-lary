2e680b9260284653e4229aaedf0a70547ec38036
Mark Hamilton <ham.mark126@gmail.com>
2022-11-29 18:59:12 -0600

    deleted starting DOM message and added login/logout to DOM, added more promises

--- api/vocabData.js
+++ api/vocabData.js
@@ -26,7 +26,34 @@ const getSingleVocab = (firebaseKey) => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const createVocab = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const deleteVocab = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
 export {
   getVocab,
   getSingleVocab,
+  createVocab,
+  deleteVocab,
 };
