583bde4e4800ca0e63d6cd1192701ff19cfc73d7
Mark Hamilton <ham.mark126@gmail.com>
2022-11-29 18:29:16 -0600

    made get promises

---
+++ api/vocabData.js
@@ -0,0 +1,32 @@
+import client from '../utils/client';
+
+const endpoint = client.databaseURL;
+
+const getVocab = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getSingleVocab = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/vocabulary/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
+export {
+  getVocab,
+  getSingleVocab,
+};
